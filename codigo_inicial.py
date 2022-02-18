7ed3ef1f711225fb5fc40e4d11cf9c2c9673225c
BOokSrc <deoliveiradiasandre@gmail.com>
2022-02-18 18:14:06 -0300

    criei a janela

---
+++ codigo_inicial.py
@@ -0,0 +1,40 @@
+import requests
+
+from tkinter import *
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dollar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+
+
+    texto_cotações["text"] = texto
+
+    #print(texto)
+
+#Início da Janela
+janela = Tk()
+janela.title("Quotes")
+janela.geometry("210x200")
+
+texto_orientação = Label( janela, text = "Click the button to view the quotes" )
+texto_orientação.grid( column=0, row=0, padx=10, pady=10)
+
+botao = Button( janela, text = "Search Quotes", command=pegar_cotacoes )
+botao.grid( column = 0, row = 1,padx=10, pady=10 )
+
+
+texto_cotações = Label(janela, text="")
+texto_cotações.grid(column=0, row=2,padx=10, pady=10)
+
+janela.mainloop()
+#Final da Janela
